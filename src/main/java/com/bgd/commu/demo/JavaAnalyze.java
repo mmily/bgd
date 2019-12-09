f7a5f1b6195a140e61cdef193a05852b5bf9cdf5
lucas <mlpabcd@sina.com>
2019-12-09 11:04:34 +0800

    java版的分析程序

---
+++ src/main/java/com/bgd/commu/demo/JavaAnalyze.java
@@ -0,0 +1,12 @@
+package com.bgd.commu.demo;
+
+/**
+ * @Author Lucas
+ * @Date 2019/12/9 11:02
+ * @Version 1.0
+ */
+public class JavaAnalyze {
+    public static void main(String[] args) {
+        System.out.println(".....");
+    }
+}
